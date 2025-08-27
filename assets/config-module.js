99e12de0aa06b8dd400c086632a9c976d52f0f78
babar395 <babarbhutta395@gmail.com>
2025-08-27 22:40:52 +0500

    assets/config-module.js

---
+++ assets/config-module.js
@@ -0,0 +1,18 @@
+// assets/config-module.js
+export const FB_CONFIG = {
+  firebaseConfig: {
+    apiKey: "AIzaSyBGVki0KdMCM9fq5u-tKjr-eHhox6YCPFQ",
+    authDomain: "subscribetofollow.firebaseapp.com",
+    databaseURL: "https://subscribetofollow-default-rtdb.firebaseio.com",
+    projectId: "subscribetofollow",
+    storageBucket: "subscribetofollow.firebasestorage.app",
+    messagingSenderId: "684835820085",
+    appId: "1:684835820085:web:0a4a4a958547c655ff977c"
+  },
+  // Ye woh blog URL hai jahan redirect hoga har 10 tasks ke baad
+  blogUrl: "https://subscribetofollow.blogspot.com/2025/08/read-and-get-start.html",
+  adminEmails: ["babarbhutta395@gmail.com"],
+  whatsappBuyLink: "https://wa.me/923136140434?text=Hello!%20I%20want%20to%20buy%20points",
+  supportEmail: "babarbhutta395@gmail.com"
+};
+export default { FB_CONFIG };
